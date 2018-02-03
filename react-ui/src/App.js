a70e6eaa32b76b8e370e2209ed4cf4f70bb5d50d
smhood <Hoodster92@gmail.com>
2018-02-03 10:19:16 -0600

    testing this baby out

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -34,16 +33,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          {this.state.fetching
+      <div>
+        This app has nothing!
+        {this.state.fetching
             ? 'Fetching message from API'
-            : this.state.message}
-        </p>
+            : this.state.message}      
       </div>
     );
   }
