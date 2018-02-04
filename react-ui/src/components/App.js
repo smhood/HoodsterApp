ecba97fe8d83c5d9745118950d90550736f88685
HOODSCO <Scott.Hood@chrobinson.com>
2018-02-03 20:14:27 -0600

    Added NavBar

--- react-ui/src/components/App.js
+++ react-ui/src/components/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './home/HomePage';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class App extends React.Component {
 
   render() {
     return (
-      <div className="container-fluid">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-        </Switch>
+      <div>
+        <Navbar inverse >
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Hoodster</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Home
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </div>
       </div>
     );
   }
