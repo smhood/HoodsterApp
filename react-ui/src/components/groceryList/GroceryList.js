007aa920deaf4432af7b679a814be1b8feec5b21
smhood <Hoodster92@gmail.com>
2018-02-06 20:03:46 -0600

    grocery list

--- react-ui/src/components/groceryList/GroceryList.js
+++ react-ui/src/components/groceryList/GroceryList.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import {ListGroup, ListGroupItem, FormControl, Button} from 'react-bootstrap';
 
 class GroceryList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          groceryList: [],
+          newItem: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.addItem = this.addItem.bind(this);
+    };
+
+    addItem(){
+        let groceryList = Object.assign([], this.state.groceryList);
+
+        groceryList.push(this.state.newItem);
+        this.setState({newItem: "", groceryList: groceryList});
+    };
+
+    handleChange(event){
+        this.setState({newItem: event.target.value});
+    };
+
     render(){
         return (
             <div className="row">
                 <div className="col-md-12">
-                    Grocery List
+                    {this.state.groceryList.map((item,i) => <li key={i}>{item}</li>)}
+                    <FormControl
+                        type="text"
+                        value={this.state.newItem}
+                        placeholder="Enter text"
+                        onChange={this.handleChange}
+                    />
+                    <Button onClick={() => this.addItem()}>Add Item</Button>
                 </div>
             </div>
         )
-    }
+    };
 }
 
 export default GroceryList;
